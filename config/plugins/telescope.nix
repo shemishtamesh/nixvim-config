50b23a9ab40888c69b4557181f461969319c7d0f
Yuval Inov <yuval@agamanalytics.com>
2025-04-24 13:28:33 +0300

    show hidden files with <leader>F

--- config/plugins/telescope.nix
+++ config/plugins/telescope.nix
@@ -26,7 +26,7 @@ in
     (keymap "n" "<leader>fo" "<cmd>Telescope oldfiles<CR>" { })
     (keymap "n" "<leader>ff" ''<cmd>Telescope frecency workspace=CWD path_display={"smart"}<CR>'' { })
     (keymap "n" "<leader>fF" "<cmd>Telescope frecency<CR>" { })
-    (keymap "n" "<leader>F" "<cmd>Telescope find_files<CR>" { })
+    (keymap "n" "<leader>F" "<cmd>Telescope find_files hidden=true<CR>" { })
     (keymap "n" "<leader>fl" "<cmd>Telescope live_grep<CR>" { })
     (keymap "n" "<leader>fg" "<cmd>Telescope git_files<CR>" { })
     (keymap "n" "<leader>fs" "<cmd>Telescope grep_string<CR>" { })
