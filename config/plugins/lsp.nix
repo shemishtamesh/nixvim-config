f8a9932e8e8648f8a5d641432842511e672dd4ff
shemishtamesh <shemishtamail@gmail.com>
2025-05-23 18:28:32 +0300

    using telescope for lsp bindings

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -1,6 +1,33 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 let
   keymap = (import ../nix_functions.nix).keymap;
+
+  telescope_commands = (
+    if config.plugins.telescope.enable then
+      {
+        workspace_symbols = "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>";
+        type_definitions = "<cmd>Telescope lsp_type_definitions<CR>";
+        references = "<cmd>Telescope lsp_references<CR>";
+        implementations = "<cmd>Telescope lsp_implementations<CR>";
+        incoming_calls = "<cmd>Telescope lsp_incoming_calls<CR>";
+        outgoing_calls = "<cmd>Telescope lsp_outgoing_calls<CR>";
+        definitions = "<cmd>Telescope lsp_definitions<CR>";
+        declarations = "<cmd>Telescope lsp_declarations<CR>";
+        diagnostics = "<cmd>Telescope diagnostics<CR>";
+      }
+    else
+      {
+        workspace_symbols = "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>";
+        type_definitions = "<cmd>lua vim.lsp.buf.type_definition()<CR>";
+        references = "<cmd>lua vim.lsp.buf.references()<CR>";
+        implementations = "<cmd>lua vim.lsp.buf.implementation()<CR>";
+        incoming_calls = "<cmd>lua vim.lsp.buf.incoming_calls()<CR>";
+        outgoing_calls = "<cmd>lua vim.lsp.buf.outgoing_calls()<CR>";
+        definitions = "<cmd>lua vim.lsp.buf.definition()<CR>";
+        declarations = "<cmd>lua vim.lsp.buf.declaration()<CR>";
+        diagnostics = "<cmd>lua vim.diagnostic.setqflist()<CR>";
+      }
+  );
 in
 {
   diagnostic.settings = {
@@ -119,52 +146,26 @@ in
   };
   keymaps = [
     # defined outside of lsp for whichkey
-    (keymap "n" "<leader>la" "<cmd>lua vim.lsp.buf.code_action()<CR>" {
-      silent = true;
-    })
-    (keymap "n" "<leader>lf" "<cmd>lua vim.lsp.buf.format()<CR>" {
-      silent = true;
-    })
-    (keymap "n" "<leader>ln" "<cmd>lua vim.lsp.buf.rename()<CR>" {
-      silent = true;
-    })
-    (keymap "n" "<leader>lm" "<cmd>lua vim.lsp.buf.implementation()<CR>" {
-      silent = true;
-    })
-    (keymap "n" "<leader>li" "<cmd>lua vim.lsp.buf.incoming_calls()<CR>" {
-      silent = true;
-    })
-    (keymap "n" "<leader>lo" "<cmd>lua vim.lsp.buf.outgoing_calls()<CR>" {
-      silent = true;
-    })
-    (keymap "n" "<leader>lr" "<cmd>lua vim.lsp.buf.references()<CR>" {
-      silent = true;
-    })
-    (keymap "n" "<leader>lh" "<cmd>lua vim.lsp.buf.signature_help()<CR>" {
-      silent = true;
-    })
-    (keymap "n" "<leader>lt" "<cmd>lua vim.lsp.buf.type_definition()<CR>" {
-      silent = true;
-    })
-    (keymap "n" "<leader>lc" "<cmd>lua vim.lsp.buf.typehierarchy()<CR>" {
-      silent = true;
-    })
-    (keymap "n" "<leader>ls" "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>" {
-      silent = true;
-    })
+    (keymap "n" "<leader>la" "<cmd>lua vim.lsp.buf.code_action()<CR>" { silent = true; })
+    (keymap "n" "<leader>lf" "<cmd>lua vim.lsp.buf.format()<CR>" { silent = true; })
+    (keymap "n" "<leader>ln" "<cmd>lua vim.lsp.buf.rename()<CR>" { silent = true; })
+    (keymap "n" "<leader>lh" "<cmd>lua vim.lsp.buf.signature_help()<CR>" { silent = true; })
+    (keymap "n" "<leader>lc" "<cmd>lua vim.lsp.buf.typehierarchy()<CR>" { silent = true; })
     (keymap "n" "<leader>ll"
       "<cmd>lua vim.diagnostic.config({virtual_lines = not vim.diagnostic.config().virtual_lines, virtual_text = not vim.diagnostic.config().virtual_text})<CR>"
       {
         silent = true;
       }
     )
-    (keymap "n" "gd" "<cmd>lua vim.lsp.buf.definition()<CR>" { silent = true; })
-    (keymap "n" "gD" "<cmd>lua vim.lsp.buf.declaration()<CR>" {
-      silent = true;
-    })
-    (keymap "n" "<leader>ld" "<cmd>lua vim.diagnostic.setqflist()<CR>" {
-      silent = true;
-    })
+    (keymap "n" "<leader>ld" telescope_commands.diagnostics { silent = true; })
+    (keymap "n" "<leader>lm" telescope_commands.implementations { silent = true; })
+    (keymap "n" "<leader>li" telescope_commands.incoming_calls { silent = true; })
+    (keymap "n" "<leader>lo" telescope_commands.outgoing_calls { silent = true; })
+    (keymap "n" "<leader>lr" telescope_commands.references { silent = true; })
+    (keymap "n" "<leader>lt" telescope_commands.type_definitions { silent = true; })
+    (keymap "n" "<leader>ls" telescope_commands.workspace_symbols { silent = true; })
+    (keymap "n" "gd" telescope_commands.definitions { silent = true; })
+    (keymap "n" "gD" telescope_commands.declarations { silent = true; })
     (keymap "n" "]d" "<cmd>lua vim.diagnostic.goto_next()<CR>" { silent = true; })
     (keymap "n" "[d" "<cmd>lua vim.diagnostic.goto_prev()<CR>" { silent = true; })
   ];
