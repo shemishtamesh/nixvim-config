d3ee62fe745ac2000336daa551c4ee9bfeae0cf6
shemishtamesh <shemishtamail@gmail.com>
2025-07-13 12:22:29 +0300

    flash autojump

--- config/plugins/flash.nix
+++ config/plugins/flash.nix
@@ -6,16 +6,11 @@
         search.enabled = false;
         char.enabled = false;
       };
+      jump.autojump = true;
     };
   };
 
   keymaps = [
-    (
-      (import ../nix_functions.nix).keymap
-      "n"
-      "<C-s>"
-      "<CMD>lua require('flash').jump()<CR>"
-      { }
-    )
+    ((import ../nix_functions.nix).keymap "n" "<C-s>" "<CMD>lua require('flash').jump()<CR>" { })
   ];
 }
