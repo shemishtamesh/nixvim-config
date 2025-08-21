0a5e086255f5d0750346c6bffa5bff5cc67eb3cd
Yuval Inov <yuval@agamanalytics.com>
2025-08-21 14:37:53 +0300

    more neogit keymaps

--- config/plugins/neogit.nix
+++ config/plugins/neogit.nix
@@ -14,8 +14,18 @@ in
   };
 
   keymaps = [
-    (keymap "n" "<leader>GS" "<cmd>Neogit<CR>" { silent = true; })
-    (keymap "n" "<leader>GL" "<cmd>NeogitLog<CR>" { silent = true; })
-    (keymap "n" "<leader>GC" "<cmd>NeogitCommit<CR>" { silent = true; })
+    (keymap "n" "<leader>gs" "<cmd>Neogit<CR>" { silent = true; })
+    (keymap "n" "<leader>gl" "<cmd>Neogit log<CR>" { silent = true; })
+    (keymap "n" "<leader>gc" "<cmd>Neogit commit<CR>" { silent = true; })
+    (keymap "n" "<leader>gw" "<cmd>Neogit worktree<CR>" { silent = true; })
+    (keymap "n" "<leader>gb" "<cmd>Neogit branch<CR>" { silent = true; })
+    (keymap "n" "<leader>gm" "<cmd>Neogit merge<CR>" { silent = true; })
+    (keymap "n" "<leader>gr" "<cmd>Neogit remote<CR>" { silent = true; })
+    (keymap "n" "<leader>gf" "<cmd>Neogit fetch<CR>" { silent = true; })
+    (keymap "n" "<leader>gp" "<cmd>Neogit pull<CR>" { silent = true; })
+    (keymap "n" "<leader>gP" "<cmd>Neogit push<CR>" { silent = true; })
+    (keymap "n" "<leader>gS" "<cmd>Neogit stash<CR>" { silent = true; })
+    (keymap "n" "<leader>gL" "<cmd>NeogitLog<CR>" { silent = true; })
+    (keymap "n" "<leader>gC" "<cmd>NeogitCommit<CR>" { silent = true; })
   ];
 }
