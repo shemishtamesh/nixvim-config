608c610ad26735d743cb19f21055cf6689e5f2b5
shemishtamesh <shemishtamail@gmail.com>
2025-08-22 10:37:35 +0300

    cleaner bare repo support

--- config/plugins/git-worktree.nix
+++ config/plugins/git-worktree.nix
@@ -17,14 +17,26 @@ in
       update_on_switch(path, prev_path)
       vim.cmd('clearjumps')
     end)
+
+    local function get_common_worktree_dir()
+      local handle =
+        io.popen("git rev-parse --path-format=absolute --git-common-dir | sed 's|\\(.*\\)/\\.git|\\1|'")
+      if handle ~= nil then
+        local result = handle:read("*a")
+        handle:close()
+        return vim.trim(result)
+      end
+      return nil
+    end
+
+    vim.keymap.set("n", "<leader>W", function()
+      local common_worktree_dir = get_common_worktree_dir()
+      require('telescope').extensions.git_worktree.create_git_worktree({ prefix = common_worktree_dir .. "/" })
+    end, { desc = "[G]it [W]orktree [C]reate" })
   '';
 
   keymaps = [
     (keymap "n" "<leader>w" "<cmd>lua require('telescope').extensions.git_worktree.git_worktree()<CR>"
-      { }
-    )
-    (keymap "n" "<leader>W"
-      "<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>"
       { }
     )
   ];
