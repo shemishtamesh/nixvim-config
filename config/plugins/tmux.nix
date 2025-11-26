8ac5026f1e90254a4577afd51fb502299259f174
Yuval Inov <yuval@agamanalytics.com>
2025-11-26 17:32:53 +0200

    tmux navigator insert mode

--- config/plugins/tmux.nix
+++ config/plugins/tmux.nix
@@ -1,4 +1,7 @@
 { pkgs, ... }:
+let
+  keymap = (import ../nix_functions.nix).keymap;
+in
 {
   plugins.tmux-navigator.enable = true;
   extraPlugins = with pkgs.vimPlugins; [ vim-tpipeline ];
@@ -15,4 +18,11 @@
       vim.o.laststatus = 1
     end
   '';
+  keymaps = [
+    (keymap "i" "<c-h>" "<c-o>:TmuxNavigateLeft<cr>" { silent = true; })
+    (keymap "i" "<c-j>" "<c-o>:TmuxNavigateDown<cr>" { silent = true; })
+    (keymap "i" "<c-k>" "<c-o>:TmuxNavigateUp<cr>" { silent = true; })
+    (keymap "i" "<c-l>" "<c-o>:TmuxNavigateRight<cr>" { silent = true; })
+    (keymap "i" "<c-\\>" "<c-o>:TmuxNavigatePrevious<cr>" { silent = true; })
+  ];
 }
