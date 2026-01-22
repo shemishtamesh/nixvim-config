7e50170ad74bb20fd41298590297daf89dcbab3f
Yuval Inov <yuval@agamanalytics.com>
2026-01-22 16:03:11 +0200

    qwen3

--- config/plugins/codecompanion.nix
+++ config/plugins/codecompanion.nix
@@ -1,50 +1,25 @@
 { lib, ... }:
+let
+  keymap = (import ../nix_functions.nix).keymap;
+in
 {
-  plugins = {
-    codecompanion = {
-      enable = true;
-      settings = {
-        opts = {
-          completion_provider = "cmp";
-          adapters = {
-            # acp = {
-            #   opencode.__raw = ''
-            #       function()
-            #         return require("codecompanion.adapters").extend("opencode", {
-            #           defaults = {
-            #           model = "ollama/devstral-small-2"
-            #         };
-            #       })
-            #     end'';
-            # };
-            # http = {
-            #   ollama.__raw = ''
-            #     function()
-            #       return require("codecompanion.adapters").extend("ollama", {
-            #         env = {
-            #           url = "https://localhost",
-            #         },
-            #         headers = {
-            #           ["Content-Type"] = "application/json",
-            #         },
-            #         parameters = {
-            #           sync = true,
-            #         },
-            #       })
-            #     end
-            #   '';
-            # };
-          };
+  plugins.codecompanion = {
+    enable = true;
+    settings = {
+      opts.completion_provider = "cmp";
+      interactions = lib.genAttrs [ "chat" "inline" "background" "cmd" ] (_: {
+        adapter = {
+          name = "ollama";
+          model = "qwen3-coder";
         };
-        interactions = lib.genAttrs [ "chat" "inline" "background" "cmd" ] (_: {
-          # adapter = "ollama";
-          adapter = "opencode";
-          # adapter = {
-          #   name = "opencode";
-          #   model = "ollama/devstral-small-2";
-          # };
-        });
-      };
+      });
+      display.action_palette.provider = "telescope";
     };
   };
+  keymaps = [
+    (keymap [ "n" "v" ] "<C-a>" "<cmd>CodeCompanion<cr>" { })
+    (keymap [ "n" "v" ] "<leader>A" "<cmd>CodeCompanionActions<cr>" { })
+    (keymap "n" "<leader>a" "<cmd>CodeCompanionChat Toggle<cr>" { })
+    (keymap "v" "<leader>a" "<cmd>CodeCompanionChat Add<cr>" { })
+  ];
 }
