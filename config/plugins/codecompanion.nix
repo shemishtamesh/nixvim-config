cf52f1befbb24687562464a7351705a9a11ba0c9
shemishtamesh <shemishtamail@gmail.com>
2026-02-02 20:58:53 +0200

    reformat

--- config/plugins/codecompanion.nix
+++ config/plugins/codecompanion.nix
@@ -27,74 +27,83 @@ in
     (keymap "ca" "CC" "CodeCompanion" { })
   ];
   extraConfigLua = ''
-      local codecompanion_extmarks_default_opts = {
-        unique_line_sign_text = "",
-        first_line_sign_text = "┌",
-        last_line_sign_text = "└",
-        extmark = {
-          sign_hl_group = "DiagnosticWarn",
-          sign_text = "│",
-          priority = 1000,
-        },
-      }
+    local codecompanion_extmarks_default_opts = {
+      unique_line_sign_text = "",
+      first_line_sign_text = "┌",
+      last_line_sign_text = "└",
+      extmark = {
+        sign_hl_group = "DiagnosticWarn",
+        sign_text = "│",
+        priority = 1000,
+      },
+    }
 
-      local function codecompanion_extmarks_set_line_extmark(bufnr, ns_id, line_num, opts, sign_type)
-        vim.api.nvim_buf_set_extmark(
-          bufnr,
-          ns_id,
-          line_num - 1,
-          0,
-          vim.tbl_deep_extend("force", opts.extmark or {}, {
-            sign_text = opts[sign_type] or opts.extmark.sign_text,
-          })
-        )
-      end
-
-      local function codecompanion_extmarks_create_extmarks(opts, data, ns_id)
-        local context = data.buffer_context
-
-        if context.start_line == context.end_line then
-          codecompanion_extmarks_set_line_extmark(context.bufnr, ns_id, context.start_line, opts, "unique_line_sign_text")
-          return
-        end
-
-        codecompanion_extmarks_set_line_extmark(context.bufnr, ns_id, context.start_line, opts, "first_line_sign_text")
-
-        for i = context.start_line + 1, context.end_line - 1 do
-          local middle = context.start_line + math.floor((context.end_line - context.start_line) / 2)
-          if i == middle then
-            codecompanion_extmarks_set_line_extmark(context.bufnr, ns_id, i, opts, "unique_line_sign_text")
-          else
-            vim.api.nvim_buf_set_extmark(context.bufnr, ns_id, i - 1, 0, opts.extmark)
-          end
-        end
-
-        if context.end_line > context.start_line then
-          codecompanion_extmarks_set_line_extmark(context.bufnr, ns_id, context.end_line, opts, "last_line_sign_text")
-        end
-      end
-
-      local function codecompanion_extmarks_create_autocmds(opts)
-        vim.api.nvim_create_autocmd({ "User" }, {
-          pattern = { "CodeCompanionRequest*" },
-          callback = function(event)
-            local data = event.data or {}
-            local context = data.buffer_context or {}
-
-            if vim.tbl_isempty(context) and data.interaction ~= "inline" then
-              return
-            end
-
-            local ns_id = vim.api.nvim_create_namespace("CodeCompanionInline_" .. data.id)
-
-            if event.match:find("Started") then
-              codecompanion_extmarks_create_extmarks(opts, data, ns_id)
-            elseif event.match:find("Finished") then
-              vim.api.nvim_buf_clear_namespace(context.bufnr, ns_id, 0, -1)
-            end
-          end,
+    --- @param bufnr number
+    --- @param ns_id number
+    --- @param line_num number
+    --- @param opts table
+    --- @param sign_type string
+    local function codecompanion_extmarks_set_line_extmark(bufnr, ns_id, line_num, opts, sign_type)
+      vim.api.nvim_buf_set_extmark(
+        bufnr,
+        ns_id,
+        line_num - 1,
+        0,
+        vim.tbl_deep_extend("force", opts.extmark or {}, {
+          sign_text = opts[sign_type] or opts.extmark.sign_text,
         })
+      )
+    end
+
+    --- @param opts table
+    --- @param data table
+    --- @param ns_id number
+    local function codecompanion_extmarks_create_extmarks(opts, data, ns_id)
+      local context = data.buffer_context
+
+      if context.start_line == context.end_line then
+        codecompanion_extmarks_set_line_extmark(context.bufnr, ns_id, context.start_line, opts, "unique_line_sign_text")
+        return
       end
+
+      codecompanion_extmarks_set_line_extmark(context.bufnr, ns_id, context.start_line, opts, "first_line_sign_text")
+
+      for i = context.start_line + 1, context.end_line - 1 do
+        local middle = context.start_line + math.floor((context.end_line - context.start_line) / 2)
+        if i == middle then
+          codecompanion_extmarks_set_line_extmark(context.bufnr, ns_id, i, opts, "unique_line_sign_text")
+        else
+          vim.api.nvim_buf_set_extmark(context.bufnr, ns_id, i - 1, 0, opts.extmark)
+        end
+      end
+
+      if context.end_line > context.start_line then
+        codecompanion_extmarks_set_line_extmark(context.bufnr, ns_id, context.end_line, opts, "last_line_sign_text")
+      end
+    end
+
+    --- @param opts table
+    local function codecompanion_extmarks_create_autocmds(opts)
+      vim.api.nvim_create_autocmd({ "User" }, {
+        pattern = { "CodeCompanionRequest*" },
+        callback = function(event)
+          local data = event.data or {}
+          local context = data.buffer_context or {}
+
+          if vim.tbl_isempty(context) and data.interaction ~= "inline" then
+            return
+          end
+
+          local ns_id = vim.api.nvim_create_namespace("CodeCompanionInline_" .. data.id)
+
+          if event.match:find("Started") then
+            codecompanion_extmarks_create_extmarks(opts, data, ns_id)
+          elseif event.match:find("Finished") then
+            vim.api.nvim_buf_clear_namespace(context.bufnr, ns_id, 0, -1)
+          end
+        end,
+      })
+    end
 
     codecompanion_extmarks_create_autocmds(vim.tbl_deep_extend("force", codecompanion_extmarks_default_opts, {}))
   '';
