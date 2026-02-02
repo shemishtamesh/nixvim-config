dee1c24ef750a3db2a60d4bc8db532a97d30a260
Yuval Inov <yuval@agamanalytics.com>
2026-02-02 10:11:43 +0200

    hover cmdline

--- config/plugins/noice-nvim.nix
+++ config/plugins/noice-nvim.nix
@@ -2,7 +2,19 @@ let
   keymap = (import ../nix_functions.nix).keymap;
 in
 {
-  plugins.noice.enable = true;
+  plugins.noice = {
+    enable = true;
+    settings = {
+      cmdline.view = "hover";
+      views.hover.border = {
+        style = "rounded";
+        padding = [
+          0
+          4
+        ];
+      };
+    };
+  };
   keymaps = [
     (keymap [ "n" ] "<leader>n" "<cmd>NoicePick<cr>" { })
     (keymap [ "n" "i" "v" ] "<M-;>" "<cmd>NoiceDismiss<cr>" { })
