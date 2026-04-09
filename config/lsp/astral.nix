316d61eed0ab5cc9fc711a6d17c1aab8db837492
Yuval Inov <yuval@agamanalytics.com>
2026-04-09 18:45:36 +0300

    fix ruff config

--- config/lsp/astral.nix
+++ config/lsp/astral.nix
@@ -8,7 +8,7 @@
         init_options = {
           settings = {
             lineLength = 79;
-
+            indent-width = 4;
             lint = {
               extendSelect = [
                 "I"
@@ -28,27 +28,23 @@
                 "ISC003"
               ];
             };
-
-            configuration = toString (
-              (pkgs.formats.toml { }).generate "ruff.toml" {
-                lint = {
-                  flake8-implicit-str-concat.allow-multiline = false;
-                  pycodestyle.max-line-length = 79;
-                  flake8-quotes = {
-                    inline-quotes = "double";
-                    multiline-quotes = "double";
-                    docstring-quotes = "double";
-                  };
+            configuration = {
+              lint = {
+                flake8-implicit-str-concat.allow-multiline = false;
+                flake8-quotes = {
+                  inline-quotes = "double";
+                  multiline-quotes = "double";
+                  docstring-quotes = "double";
                 };
-                format = {
-                  quote-style = "double";
-                  indent-width = 4;
-                  line-ending = "lf";
-                  skip-magic-trailing-comma = false;
-                  docstring-code-format = true;
-                };
-              }
-            );
+                pycodestyle.max-line-length = 79;
+              };
+              format = {
+                quote-style = "double";
+                line-ending = "lf";
+                skip-magic-trailing-comma = false;
+                docstring-code-format = true;
+              };
+            };
           };
         };
       };
