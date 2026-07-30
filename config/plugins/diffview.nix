80d92b9ac757476c0197f4a366837f8929912c09
Yuval Inov <yuval@agamanalytics.com>
2026-07-30 09:30:11 +0300

    add quit keymap for diffview

--- config/plugins/diffview.nix
+++ config/plugins/diffview.nix
@@ -1,14 +1,33 @@
 { utils, ... }:
 {
-  plugins.diffview.enable = true;
+  plugins.diffview = {
+    enable = true;
+    settings.keymaps =
+      let
+        quitBinding = {
+          mode = "n";
+          key = "q";
+          action = "<cmd>DiffviewClose<cr>";
+          description = "Close diffview";
+        };
+      in
+      {
+        view = [ quitBinding ];
+        file_panel = [ quitBinding ];
+      };
+  };
 
   keymaps = [
     (utils.map "n" "<leader>vo" "<cmd>DiffviewOpen<cr>" { desc = "Diffview open"; })
     (utils.map "n" "<leader>vc" "<cmd>DiffviewClose<cr>" { desc = "Diffview close"; })
     (utils.map "n" "<leader>vr" "<cmd>DiffviewRefresh<cr>" { desc = "Diffview refresh"; })
     (utils.map "n" "<leader>vf" "<cmd>DiffviewToggleFiles<cr>" { desc = "Diffview toggle file panel"; })
-    (utils.map "n" "<leader>vh" "<cmd>DiffviewFileHistory %<cr>" { desc = "Diffview file history (current file)"; })
-    (utils.map "n" "<leader>vH" "<cmd>DiffviewFileHistory<cr>" { desc = "Diffview file history (repo)"; })
+    (utils.map "n" "<leader>vh" "<cmd>DiffviewFileHistory %<cr>" {
+      desc = "Diffview file history (current file)";
+    })
+    (utils.map "n" "<leader>vH" "<cmd>DiffviewFileHistory<cr>" {
+      desc = "Diffview file history (repo)";
+    })
     (utils.map "ca" "DV" "DiffviewOpen" { })
   ];
 }
