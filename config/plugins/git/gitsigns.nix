2e0139fb03f19627c62e3d83744ce055dd882dda
shemishtamesh <shemishtamail@gmail.com>
2026-08-18 20:33:10 +0300

    make gitsigns commands silent

--- config/plugins/git/gitsigns.nix
+++ config/plugins/git/gitsigns.nix
@@ -18,30 +18,84 @@
   };
 
   keymaps = [
-    (utils.map "n" "[G" "<cmd>Gitsigns nav_hunk first<cr>" { desc = "First hunk"; })
-    (utils.map "n" "[g" "<cmd>Gitsigns nav_hunk prev<cr>" { desc = "Previous hunk"; })
-    (utils.map "n" "]g" "<cmd>Gitsigns nav_hunk next<cr>" { desc = "Next hunk"; })
-    (utils.map "n" "]G" "<cmd>Gitsigns nav_hunk last<cr>" { desc = "Last hunk"; })
+    (utils.map "n" "[G" "<cmd>Gitsigns nav_hunk first<cr>" {
+      desc = "First hunk";
+      silent = true;
+    })
+    (utils.map "n" "[g" "<cmd>Gitsigns nav_hunk prev<cr>" {
+      desc = "Previous hunk";
+      silent = true;
+    })
+    (utils.map "n" "]g" "<cmd>Gitsigns nav_hunk next<cr>" {
+      desc = "Next hunk";
+      silent = true;
+    })
+    (utils.map "n" "]G" "<cmd>Gitsigns nav_hunk last<cr>" {
+      desc = "Last hunk";
+      silent = true;
+    })
 
-    (utils.map "n" "<leader>Gv" "<cmd>Gitsigns preview_hunk<cr>" { desc = "Preview hunk"; })
-    (utils.map "n" "<leader>GV" "<cmd>Gitsigns preview_hunk_inline<cr>" { desc = "Preview hunk (inline)"; })
+    (utils.map "n" "<leader>Gv" "<cmd>Gitsigns preview_hunk<cr>" {
+      desc = "Preview hunk";
+      silent = true;
+    })
+    (utils.map "n" "<leader>GV" "<cmd>Gitsigns preview_hunk_inline<cr>" {
+      desc = "Preview hunk (inline)";
+      silent = true;
+    })
 
-    (utils.map "n" "<leader>Gs" "<cmd>Gitsigns select_hunk<cr>" { desc = "Select hunk"; })
+    (utils.map "n" "<leader>Gs" "<cmd>Gitsigns select_hunk<cr>" {
+      desc = "Select hunk";
+      silent = true;
+    })
 
-    (utils.map "n" "<leader>GR" "<cmd>Gitsigns reset_buffer<cr>" { desc = "Reset buffer"; })
-    (utils.map [ "n" "v" ] "<leader>Gr" ":Gitsigns reset_hunk<cr>" { desc = "Reset hunk"; })
-    (utils.map [ "n" "v" ] "<leader>Ga" ":Gitsigns stage_hunk<cr>" { desc = "Stage hunk"; })
-    (utils.map "n" "<leader>GA" "<cmd>Gitsigns stage_buffer<cr>" { desc = "Stage buffer"; })
+    (utils.map "n" "<leader>GR" "<cmd>Gitsigns reset_buffer<cr>" {
+      desc = "Reset buffer";
+      silent = true;
+    })
+    (utils.map [ "n" "v" ] "<leader>Gr" ":Gitsigns reset_hunk<cr>" {
+      desc = "Reset hunk";
+      silent = true;
+    })
+    (utils.map [ "n" "v" ] "<leader>Ga" ":Gitsigns stage_hunk<cr>" {
+      desc = "Stage hunk";
+      silent = true;
+    })
+    (utils.map "n" "<leader>GA" "<cmd>Gitsigns stage_buffer<cr>" {
+      desc = "Stage buffer";
+      silent = true;
+    })
 
-    (utils.map "n" "<leader>Gw" "<cmd>Gitsigns toggle_word_diff<cr>" { desc = "Toggle word diff"; })
+    (utils.map "n" "<leader>Gw" "<cmd>Gitsigns toggle_word_diff<cr>" {
+      desc = "Toggle word diff";
+      silent = true;
+    })
 
-    (utils.map "n" "<leader>Gb" "<cmd>Gitsigns blame_line<cr>" { desc = "Blame line"; })
-    (utils.map "n" "<leader>GB" "<cmd>Gitsigns blame<cr>" { desc = "Blame file"; })
-    (utils.map "n" "<leader>Gl" "<cmd>Gitsigns toggle_current_line_blame<cr>" { desc = "Toggle line blame"; })
+    (utils.map "n" "<leader>Gb" "<cmd>Gitsigns blame_line<cr>" {
+      desc = "Blame line";
+      silent = true;
+    })
+    (utils.map "n" "<leader>GB" "<cmd>Gitsigns blame<cr>" {
+      desc = "Blame file";
+      silent = true;
+    })
+    (utils.map "n" "<leader>Gl" "<cmd>Gitsigns toggle_current_line_blame<cr>" {
+      desc = "Toggle line blame";
+      silent = true;
+    })
 
-    (utils.map "n" "<leader>Gq" "<cmd>Gitsigns setqflist<cr>" { desc = "Add hunks to quickfix"; })
+    (utils.map "n" "<leader>Gq" "<cmd>Gitsigns setqflist<cr>" {
+      desc = "Add hunks to quickfix";
+      silent = true;
+    })
 
-    (utils.map "n" "<leader>Gd" "<cmd>Gitsigns diffthis vertical=true split=rightbelow<cr>" { desc = "Diff working tree"; })
-    (utils.map "n" "<leader>GD" "<cmd>Gitsigns diffthis ~1 vertical=true split=rightbelow<cr>" { desc = "Diff against HEAD~1"; })
+    (utils.map "n" "<leader>Gd" "<cmd>Gitsigns diffthis vertical=true split=rightbelow<cr>" {
+      desc = "Diff working tree";
+      silent = true;
+    })
+    (utils.map "n" "<leader>GD" "<cmd>Gitsigns diffthis ~1 vertical=true split=rightbelow<cr>" {
+      desc = "Diff against HEAD~1";
+      silent = true;
+    })
   ];
 }
